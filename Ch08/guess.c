1610813617a0b6b42777f967d8da8351a73a64a9
pengjinfei <pengjinfei@126.com>
2017-02-09 11:40:36 +0800

    add cmakelists for ch08 ch09

--- Ch08/guess.c
+++ Ch08/guess.c
@@ -1,16 +1,23 @@
 /* guess.c -- an inefficient and faulty number-guesser */
 #include <stdio.h>
-int main(void)
-{
+
+int main(void) {
     int guess = 1;
-    
+
     printf("Pick an integer from 1 to 100. I will try to guess ");
     printf("it.\nRespond with a y if my guess is right and with");
     printf("\nan n if it is wrong.\n");
     printf("Uh...is your number %d?\n", guess);
-    while (getchar() != 'y')      /* get response, compare to y */
-        printf("Well, then, is it %d?\n", ++guess);
+    char response;
+    while ((response = getchar()) != 'y') {
+        if (response == 'n')
+            printf("Well, then, is it %d?\n", ++guess);
+        else
+            printf("Sorry, I understand only y and n.\n");
+        while (getchar()!='\n')  /*跳过剩余的输入行*/
+            continue;
+    }
     printf("I knew I could do it!\n");
-    
+
     return 0;
 }
